a2150355c03c5a49b020a512f0e561e1e1803a97
Mauro Swincik Jr <mauro.sergio@itlean.com.br>
2022-03-11 13:53:08 -0300

    testing reusing workflows

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
     return res.status(200).json({
-        message: 'Esta é uma aplicação para teste - alterado.'
+        message: 'Esta é uma aplicação para teste.'
     })
 }

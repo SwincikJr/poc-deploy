a146a796ae90689975b4050cb045b9e95df3bc72
Mauro Swincik Jr <mauro.sergio@itlean.com.br>
2022-03-11 17:49:14 -0300

    testing gcloud

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
     return res.status(200).json({
-        message: 'Esta é uma aplicação para teste. - v2'
+        message: 'Esta é uma aplicação para teste. - v3'
     })
 }

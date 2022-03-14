b4042aa7ffe4de891f98a74462c4275a6b15117e
Mauro Swincik Jr <mauro.sergio@itlean.com.br>
2022-03-14 16:32:59 -0300

    testing env veriables

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,6 @@
 module.exports = (req, res) => {
     return res.status(200).json({
-        message: 'Esta é uma aplicação para testes de deploy. - v9'
+        message: `Esta é uma aplicação para testes de deploy. - v10`,
+        env: process.env.NODE_ENV
     })
 }
--- tests/integration/app.spec.js
+++ tests/integration/app.spec.js
@@ -5,6 +5,6 @@ describe('app integration test', () => {
     it('test GET / success', async () => {
         const response = await request(app).get('/')
         expect(response.statusCode).toBe(200)
-        expect(response.body.message).toBe('Esta é uma aplicação para testes de deploy. - v9')
+        expect(response.body.message).toBe('Esta é uma aplicação para testes de deploy. - v10')
     })
 })
